5f0a6effec80a94ef4356e1a5c157f09a5c90e97
elsafri <116935635+elsafri@users.noreply.github.com>
2024-02-03 12:44:36 +0300

    Update task.js

---
+++ js-features/countdown/task.js
@@ -0,0 +1,19 @@
+const timer = document.getElementById('timer');
+
+let time = timer.textContent;
+
+const formatTime = new Date();
+formatTime.setTime(time * 1000 + formatTime.getTimezoneOffset() * 60000);
+
+const setTimer = function() {
+  time--;
+  if (time) {
+    formatTime.setTime(formatTime.getTime() - 1000);
+    timer.textContent = formatTime.toLocaleTimeString('ru');
+  } else {
+    clearInterval(timerID);
+    alert('Вы победили в конкурсе!');
+}
+}
+
+const timerID = setInterval(setTimer, 1000);
